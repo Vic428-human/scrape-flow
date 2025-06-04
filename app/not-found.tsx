7d2753133fb513e242df4785a2fb436872157e62
Vic428-human <z0983195379a@gmail.com>
2025-06-04 08:31:45 +0800

    對查無此頁組件切版

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h2 className="text-6xl font-extrabold text-primary">404</h2>
+        <p className="text-2xl font-bold text-primary">當前頁面不存在</p>
+        <Link href="/" className="text-muted-foreground">
+          點我回首頁
+        </Link>
+      </div>
     </div>
   );
 }
