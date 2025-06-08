ec61aa05fc896bc61ae7ea0bf7175109b1622c90
Vic428-human <z0983195379a@gmail.com>
2025-06-09 02:11:37 +0800

    引用react query provider跟相關實例

--- components/provider/AppProvider.tsx
+++ components/provider/AppProvider.tsx
@@ -2,11 +2,32 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Create a theme provider
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+          },
+        },
+      })
+  );
+
+  return (
+    <NextThemesProvider {...props}>
+      {/* 建立一個QueryClient實例，並透過 QueryClientProvider 將其提交給應用程式所需的根元件 */}
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </NextThemesProvider>
+  );
 }
