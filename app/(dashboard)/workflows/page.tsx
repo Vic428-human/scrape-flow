4de40cf50ba18d4a259d879c07b6b92c59b69c9f
Vic428-human <z0983195379a@gmail.com>
2025-06-09 02:11:01 +0800

    添加新增流程ui 2. 但判與資料庫交互錯誤的訊息判斷跟資料庫中不存在workflow數據時的判斷

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -4,6 +4,8 @@ import { waitFor } from "@/hooks/helper/waitFor";
 import React, { Suspense } from "react";
 import { AlertCircleIcon, CheckCircle2Icon, PopcornIcon } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
 
 function page() {
   return (
@@ -13,6 +15,10 @@ function page() {
           <h1 className="text-2xl font-bold">自動化流程</h1>
           <p className="text-muted-foreground">在此建立並管理您的流程</p>
         </div>
+        <Button variant="default" className="gap-2">
+          <PlusIcon size={16} />
+          新增流程
+        </Button>
       </div>
       <div className="h-full py-6">
         <Suspense fallback={<UserWorkflowsSkeleton />}>
@@ -34,22 +40,35 @@ function UserWorkflowsSkeleton() {
 }
 
 async function UserWorkflows() {
+  let workflows = null;
+  let error: string | null = null;
   await waitFor(3000); // 假設 waitFor 是個延遲函式，延遲 3 秒
-  const workflows = await getWorkflowsForUser();
+
+  workflows = await getWorkflowsForUser();
   if (!workflows) {
     return (
       <Alert variant="destructive">
-        <AlertCircleIcon />
-        <AlertTitle>Unable to process your payment.</AlertTitle>
-        <AlertDescription>
-          <p>Please verify your billing information and try again.</p>
-          <ul className="list-inside list-disc text-sm">
-            <li>Check your card details</li>
-            <li>Ensure sufficient funds</li>
-            <li>Verify billing address</li>
-          </ul>
-        </AlertDescription>
+        <AlertCircleIcon className="w-4 h-4" />
+        <AlertTitle>發生錯誤</AlertTitle>
+        <AlertDescription>無法載入您的流程。請稍後再試。</AlertDescription>
       </Alert>
+    );
+  }
+
+  if (workflows.length === 0) {
+    // 空陣列代表 db沒查到資料
+    return (
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <div className="rounded-full bg-accent w-20 h-20 flex items-center justify-center">
+          <PopcornIcon size={40} className="stroke-primary" />
+        </div>
+        <div className="flex flex-col gap-1 text-center">
+          <p className="font-bold">目前沒有任何流程</p>
+          <p className="text-muted-foreground">
+            您可以點擊右上角的「新增流程」按鈕來建立新的流程。
+          </p>
+        </div>
+      </div>
     );
   }
   return <div></div>;
